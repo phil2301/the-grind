fbc9f6a351d3c6cd3781b8293375700db52f3144
Philip August Hyltoft <philip.hyltoft@gmail.com>
2018-04-23 22:14:45 +0200

    Final draft

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp            = require('gulp'),
     sass            = require('gulp-sass'),
     autoprefixer    = require('gulp-autoprefixer'),
+    notify          = require('gulp-notify'),
+    plumber         = require('gulp-plumber'),
+    maps            = require('gulp-sourcemaps'),
     browserSync     = require('browser-sync').create(); //laver (create) en instans af det
 
 gulp.task('styles', function() {
     gulp.src('./scss/style.scss')
+        .pipe(plumber ({ errorHandler : (err) => {
+            notify.onError({
+                title: "Gulp error in " + err.plugin,
+                message: err.toString()
+            })(err);
+        }}))
+        .pipe(maps.init())
         .pipe(sass())
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"],
+            cascade: false
+        }))
+        .pipe(maps.write("./"))
         .pipe(gulp.dest('./'))
         .pipe(browserSync.reload({stream: true}));
 });
